e4a058067dbd245f12292d7380584c59dc77f170
Bear Kent <davidbearkent@gmail.com>
2022-01-15 14:44:45 -0700

    Made data go into database, and then be pulled into an html website.

--- Views.py
+++ Views.py
@@ -68,15 +68,26 @@ def item_submit():
     message = ""
     date_missed = form.date_missed.data
     image_link = form.image_link.data    
+    
+    table_insert(date_missed, image_link)
+    redirect("/home_page")
+   
 
     # redirect("/home_page")
     return render_template("index.html", form=form, message=message)
 
-# @views.route("/home_page")
-# def home_page():
-#     for row in cur.execute('SELECT * FROM items ORDER BY date_missed'):
-#         print(row)
-#     return render_template("home_page.html")
+@views.route("/home_page")
+def home_page():
+    
+    con=get_con()
+    cur = con.cursor()
+    
+    date_misseds = []
+    item_links = []
+    
+    for row in cur.execute('SELECT * FROM items ORDER BY date_missed'):
+        return
+    return render_template("home_page.html")
 
 @views.route("/print_table")
 def print_table():
@@ -90,3 +101,21 @@ def print_table():
         rst += f"{row}\n"
         
     return rst
+
+@views.route("/print_table2")
+def print_table2():
+    con=get_con()
+    cur = con.cursor()
+    date_misseds = []
+    item_links = []
+    items = []
+    
+    rst = "TABLE DATA\n"
+    
+    for row in cur.execute('SELECT * FROM items ORDER BY date_missed'):
+        items.append(row)
+    
+    # for item in items:
+    #     print(item)
+    
+    return render_template("home_page.html", len = len(items), items = items)
